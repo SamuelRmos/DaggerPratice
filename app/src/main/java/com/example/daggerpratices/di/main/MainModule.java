df03337afdaea493bd896b6a537469c3dbaf1f00
root <root@localhost.localdomain>
2019-08-28 21:22:56 -0400

    Adding RecyclerView Adapter

--- app/src/main/java/com/example/daggerpratices/di/main/MainModule.java
+++ app/src/main/java/com/example/daggerpratices/di/main/MainModule.java
@@ -1,12 +1,19 @@
 package com.example.daggerpratices.di.main;
 
 import com.example.daggerpratices.network.main.MainApi;
+import com.example.daggerpratices.ui.main.posts.PostsRecyclerAdapter;
+
 import dagger.Module;
 import dagger.Provides;
 import retrofit2.Retrofit;
 
-@Module33
+@Module
 public class MainModule {
+
+    @Provides
+    static PostsRecyclerAdapter provideAdapter(){
+        return new PostsRecyclerAdapter();
+    }
 
     @Provides
     static MainApi provideMainApi(Retrofit retrofit){
--- app/src/main/java/com/example/daggerpratices/ui/main/posts/PostsFragments.java
+++ app/src/main/java/com/example/daggerpratices/ui/main/posts/PostsFragments.java
@@ -1,16 +1,25 @@
 package com.example.daggerpratices.ui.main.posts;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.daggerpratices.R;
+import com.example.daggerpratices.models.Post;
+import com.example.daggerpratices.ui.main.Resource;
+import com.example.daggerpratices.utils.VerticalSpacingItemDecoration;
 import com.example.daggerpratices.viewmodels.ViewModelProviderFactory;
+
+import java.util.List;
+
 import dagger.android.support.DaggerFragment;
 
 import javax.inject.Inject;
@@ -21,6 +30,10 @@ public class PostsFragments extends DaggerFragment {
 
     private PostsViewModel viewModel;
     private RecyclerView recyclerView;
+
+
+    @Inject
+    PostsRecyclerAdapter adapter;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -35,5 +48,40 @@ public class PostsFragments extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         recyclerView = view.findViewById(R.id.recycler_view);
         viewModel = ViewModelProviders.of(this, providerFactory).get(PostsViewModel.class);
+        initRecyclerView();
+        subscribeObserves();
+    }
+
+    private void subscribeObserves(){
+        viewModel.observePosts().removeObservers(getViewLifecycleOwner());
+        viewModel.observePosts().observe(getViewLifecycleOwner(), new Observer<Resource<List<Post>>>() {
+            @Override
+            public void onChanged(Resource<List<Post>> listResource) {
+                if (listResource != null){
+                    switch (listResource.status){
+                        case LOADING:{
+                            Log.d(TAG, "onChanged: LOADING...");
+                        
+                        }
+                        case SUCCESS:{
+                            Log.d(TAG, "onChanged: got posts...");
+                            adapter.setPosts(listResource.data);
+                            break;
+                        }
+                        case ERROR:{
+                            Log.e(TAG, "onChanged: ERROR..." + listResource.message );
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void initRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        VerticalSpacingItemDecoration itemDecoration = new VerticalSpacingItemDecoration(15);
+        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.setAdapter(adapter);
     }
 }
