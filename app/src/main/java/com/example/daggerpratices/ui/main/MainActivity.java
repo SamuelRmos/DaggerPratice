c4bc3e946f6fe69ff053152010f3694425848d3a
root <root@localhost.localdomain>
2019-08-29 22:13:39 -0400

    Adding Navigation Bar

--- app/src/main/java/com/example/daggerpratices/ui/main/MainActivity.java
+++ app/src/main/java/com/example/daggerpratices/ui/main/MainActivity.java
@@ -5,25 +5,36 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.example.daggerpratices.BaseActivity;
 import com.example.daggerpratices.R;
 import com.example.daggerpratices.ui.main.posts.PostsFragment;
 import com.example.daggerpratices.ui.main.profile.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final  String TAG = "MainActivity";
+    private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         testFragment();
     }
 
-    private void testFragment(){
+    private void testFragment() {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.main_container, new PostsFragment())
                 .commit();
@@ -38,13 +49,31 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
 
-            case R.id.logout:{
+            case R.id.logout: {
                 sessionManager.logOut();
                 return true;
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+
+            case R.id.nav_profile:{
+                break;
+            }
+            case R.id.nav_posts:{
+                break;
+            }
+        }
+        
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
